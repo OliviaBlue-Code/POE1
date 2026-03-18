5cdf2c0192c924da7af4ccbdcd3fca205dc73eb3
OliviaBlue-Code <ST10492120@rcconnect.edu.za>
2026-03-18 09:17:55 +0200

    added open voice greeting

--- Program.cs
+++ Program.cs
@@ -10,8 +10,37 @@ namespace POE1
 {
     internal class Program
     {
+        private string recordPath;
+
         static void Main(string[] args)
         {
+
         }
+
+        public Program()
+        {
+            // Setup the audio path
+            string path_directory = AppDomain.CurrentDomain.BaseDirectory;
+            // audio file 
+            recordPath = Path.Combine(path_directory, "C:\\Users\\Student\\source\\repos\\Part1_POE\\PTT-20260315-WA0023 (online-audio-converter.com).wav");
+        }
+          
+
+        public void PlayVoice()
+        {
+            try
+            {
+                using (SoundPlayer speechObj = new SoundPlayer(recordPath))
+                {
+                    speechObj.PlaySync();
+                }
+            }
+            catch (Exception error)
+            {
+                WriteLine($"Error. Failed to play audio {error.Message}");
+            }
+        }
+
+
     }
 }
